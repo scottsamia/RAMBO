d40cbdd82be3231d638ac7ca5697743ab38e1fb1
Scott Samia <scottsamia@Neelas-MBP.home>
2015-07-18 00:51:17 -0400

    Ready for Alpha

---
+++ RAMBO/MyURLProtocol.swift
@@ -0,0 +1,71 @@
+//
+//  MyURLProtocol.swift
+//  RAMBO
+//
+//  Created by Scott Samia on 7/14/15.
+//  Copyright (c) 2015 Scott Samia. All rights reserved.
+//
+
+import UIKit
+
+var requestCount = 0
+
+class MyURLProtocol: NSURLProtocol {
+    
+    var connection: NSURLConnection!
+    
+    override class func canInitWithRequest(request: NSURLRequest) -> Bool {
+        println("Request #\(requestCount++): URL = \(request.URL!.absoluteString)")
+        
+        if NSURLProtocol.propertyForKey("MyURLProtocolHandledKey", inRequest: request) != nil {
+            return false
+        }
+        
+        return true
+    }
+    override class func canonicalRequestForRequest(request: NSURLRequest) -> NSURLRequest {
+        return request
+    }
+    
+    override class func requestIsCacheEquivalent(aRequest: NSURLRequest,
+        toRequest bRequest: NSURLRequest) -> Bool {
+            return super.requestIsCacheEquivalent(aRequest, toRequest:bRequest)
+    }
+    
+    override func startLoading() {
+        var newRequest = self.request.mutableCopy() as! NSMutableURLRequest
+        NSURLProtocol.setProperty(true, forKey: "MyURLProtocolHandledKey", inRequest: newRequest)
+        
+        self.connection = NSURLConnection(request: newRequest, delegate: self)
+    }
+    
+    override func stopLoading() {
+        if self.connection != nil {
+            self.connection.cancel()
+        }
+        self.connection = nil
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        self.client!.URLProtocol(self, didReceiveResponse: response, cacheStoragePolicy: .NotAllowed)
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        self.client!.URLProtocol(self, didLoadData: data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        self.client!.URLProtocolDidFinishLoading(self)
+    }
+    
+    func connection(connection: NSURLConnection!, didFailWithError error: NSError!) {
+        self.client!.URLProtocol(self, didFailWithError: error)
+    }
+    
+    func connection(connection: NSURLConnection,
+        willSendRequestForAuthenticationChallenge challenge: NSURLAuthenticationChallenge) {
+            
+            println("WillSendAuthChallenge")
+            challenge.sender.useCredential(NSURLCredential(forTrust: challenge.protectionSpace.serverTrust), forAuthenticationChallenge: challenge)
+    }
+}
--- RAMBO/ViewController.swift
+++ RAMBO/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate, NSURLConnectionDelegate, DTDeviceDelegate {
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var addressTxb: UITextField!
+    @IBOutlet weak var msgDisplay: UILabel!
+    var lastURL: String = ""
+    var refreshControl:UIRefreshControl!
+    let scanner: DTDevices = DTDevices()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        webView.delegate = self
+        scanner.delegate = self
+        
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        self.refreshControl.addTarget(self, action: "refresh:", forControlEvents: UIControlEvents.ValueChanged)
+        self.webView.scrollView.addSubview(refreshControl)
+        self.webView.scrollView.contentInset = UIEdgeInsets(top: 20,left: 0,bottom: 0,right: 0)
+        self.webView.scrollView.backgroundColor = UIColor.clearColor()
+        self.webView.backgroundColor = UIColor.clearColor()
+
+        lastURL = "https://rambo.rogers-corp.com"
+        
+        var url = NSURL(string: lastURL)
+        var request = NSURLRequest(URL:url!)
+        
+   
+        webView.loadRequest(request)
+        
+        println("Web View created")
+        scanner.connect()
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +52,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func webViewDidStartLoad(webView: UIWebView) {
+        println("Web View Started Loading")
+        activityIndicator.startAnimating()
+    }
+    func webViewDidFinishLoad(webView: UIWebView) {
+        println("Web View Finished Loading")
+        activityIndicator.stopAnimating()
+    }
+    
+    func barcodeData(barcode: String!, type: Int32) {
+        msgDisplay.text = barcode + " (" + type.description + ")"
+        let script = "adaptiscanBarcode('" + barcode + "','" + type.description + "')"
+        if let returnedString = webView.stringByEvaluatingJavaScriptFromString(script) {
+            msgDisplay.text = msgDisplay.text! + " Sent to RAMBO"
+        }
 
+    }
+    
+    func deviceButtonPressed(which: Int32) {
+        msgDisplay.text = "Button " + which.description + " Press"
+        
+                switch UInt32(scanner.connstate) {
+                case CONN_CONNECTED.value:
+                    msgDisplay.text = "Connected"
+                case CONN_CONNECTING.value:
+                    msgDisplay.text = "Connecting"
+                case CONN_DISCONNECTED.value:
+                    msgDisplay.text = "Disconnected"
+                default:
+                    msgDisplay.text = ""
+                }
+    }
+    
+    func refresh(sender:UIRefreshControl)
+    {
+            webView.reload()
+            sender.endRefreshing()
+    }
+    
+
+    @IBAction func scanButtonUp(sender: AnyObject) {
+        scanner.barcodeStartScan(nil)
+    }
+    @IBAction func scanButtonDown(sender: AnyObject) {
+        scanner.barcodeStopScan(nil)
+    }
+    
 }
 
